5ac2878db90e20943bc8d5028148d5ff6937cd18
chrisruk <chrisrichardsonuk@gmail.com>
2020-06-17 11:51:44 +0100

    extract base64, to find large chunks of data

--- asar_investigate.py
+++ asar_investigate.py
@@ -1,12 +1,40 @@
 #!/usr/bin/python3
-
+import os
+import io
+import re
+import mmap
+import base64
 import collections
 import argparse
   
-def process(jsin):
-    f = open(jsin,encoding='utf-8')
+def process(jsin,base64out):
     # Find base64 data, ...
+    
+    #f = open(jsin,'rb')    
+    #reader = io.BufferedReader(f)
+    #wrapper = io.TextIOWrapper(reader)
+    #wrapper.read()
 
+    f = open(jsin,'rb')
+    dat = mmap.mmap(f.fileno(), 0, access=mmap.ACCESS_READ)
+    counter = 1
+    pattern = re.compile(br'(base64,([a-zA-Z0-9\+\/]+=*))',re.MULTILINE)
+
+    for m in pattern.finditer(dat):
+
+        start = m.start()
+        end = m.end()
+        base64dat = m.group(2)
+        #print(dat[start:start+30])
+
+        try:
+            tmp = open(os.path.join(base64out,"%d.out" % counter),'wb')
+            tmp.write(base64.b64decode(base64dat))
+            tmp.close()
+        except Exception:
+            nop = 0
+
+        counter += 1
 
 if __name__ == "__main__":
 
@@ -19,6 +47,12 @@ if __name__ == "__main__":
         help="JS to investigate",
         required=True,
     )
-
+    parser.add_argument(
+        "--out",
+        dest="base64out",
+        type=str,
+        help="Directory to output base64 decoded data",
+        required=True,
+    )
     args = parser.parse_args()
-    process(args.jsin)
+    process(args.jsin,args.base64out)
